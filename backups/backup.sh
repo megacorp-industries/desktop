4e8b4bdfbe8a84bb8defb95c3c755af8c21413c6
megacorp-industries <121791047+megacorp-industries@users.noreply.github.com>
2023-05-27 15:59:28 +0930

    Update backup.sh

--- backups/backup.sh
+++ backups/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-backup_folder=$(date +'%m-%d-%Y')
+backup_folder=$(date +'%Y-%m-%d')
 rsync -azr $HOME/.ssh $HOME/Documents $HOME/Keys $HOME/Ansible $HOME/Git /var/opt/Backups/${HOSTNAME}_${backup_folder}
